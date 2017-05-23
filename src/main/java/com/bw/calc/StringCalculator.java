a5072185cc5c5867ca357b23654568334d0456b1
Byungwook Lee <quddnr153@gmail.com>
2017-05-23 23:47:38 +0900

    [Requirements] Exception
    - Requirements 3: When calculator get negative value, throw RuntimeException.

--- src/main/java/com/bw/calc/StringCalculator.java
+++ src/main/java/com/bw/calc/StringCalculator.java
@@ -13,7 +13,6 @@ import org.apache.commons.lang3.StringUtils;
 abstract public class StringCalculator {
 	private String value = "";
 	private String separatorChars = ",:";
-	private int result = 0;
 
 	public StringCalculator() {}
 
@@ -30,16 +29,27 @@ abstract public class StringCalculator {
 		return operator(separateStringToInt());
 	}
 
-	protected List<Integer> separateStringToInt() {
+	protected List<Integer> separateStringToInt() throws RuntimeException {
 		List<Integer> intValues = new ArrayList<Integer>();
 		List<String> stringValues = Arrays.asList(StringUtils.split(value, separatorChars));
 
 		for (String stringValue : stringValues) {
+			Integer intValue = Integer.parseInt(stringValue);
+
+			if (intValue < 0) {
+				throw new RuntimeException("음수가 들어오면 RuntimeException을 던지라는 요구사항이 있어.");
+			}
+
 			intValues.add(Integer.parseInt(stringValue));
 		}
 
 		return intValues;
 	}
 
+	/**
+	 * 덧셈, 곱셈, 나눗셈 등 구현하고자하는 연산자로 구현하시오.
+	 * @param intValues
+	 * @return
+	 */
 	abstract protected int operator(final List<Integer> intValues);
 }
--- src/test/java/com/bw/calc/StringCalculatorTest.java
+++ src/test/java/com/bw/calc/StringCalculatorTest.java
@@ -1,5 +1,6 @@
 package com.bw.calc;
 
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
@@ -54,5 +55,53 @@ public class StringCalculatorTest {
 
 		// Then
 		assertEquals("value를 separator로 나눈 값은 기대값과 같아야 한다.", expected, actual);
+	}
+
+	@Test(expected = RuntimeException.class)
+	public void testSeparateStringToInt_음수_예외처리() {
+		// Given
+		List<Integer> expected = new ArrayList<Integer>();
+		expected.add(1);
+		expected.add(2);
+		expected.add(3);
+		String value = "//1,2:\n3;-4";
+		String addSeparatorChars = "//\n";
+
+		// When
+		List<Integer> actual = (new StringCalculator(value, addSeparatorChars) {
+			@Override
+			protected int operator(List<Integer> intValues) {
+				return 0;
+			}
+		}).separateStringToInt();
+
+		// Then
+		assertEquals("value를 separator로 나눈 값은 기대값과 같아야 한다.", expected, actual);
+	}
+
+	@Test
+	public void testSeparateStringToInt_음수_try_catch() {
+		// Given
+		List<Integer> expected = new ArrayList<Integer>();
+		expected.add(1);
+		expected.add(2);
+		expected.add(3);
+		String value = "//1,2:\n3;-4";
+		String addSeparatorChars = "//\n;";
+
+		// When
+		try {
+			List<Integer> actual = (new StringCalculator(value, addSeparatorChars) {
+				@Override
+				protected int operator(List<Integer> intValues) {
+					return 0;
+				}
+			}).separateStringToInt();
+			// Then
+			fail("Expected an RuntimeException to be thrown");
+		} catch (RuntimeException runtimeException) {
+			// Then
+			assertThat(runtimeException.getMessage(), is("음수가 들어오면 RuntimeException을 던지라는 요구사항이 있어."));
+		}
 	}
 }
