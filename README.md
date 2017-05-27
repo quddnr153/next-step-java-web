956b1958eecde67f499e2b9d404ad4a832ed6352
Byungwook Lee <byungwook.lee@nhn.com>
2017-05-27 13:12:52 +0900

    [Project setting] Web app
    - Set initial project

--- README.md
+++ README.md
@@ -4,5 +4,5 @@
 Study plan
 - 각 Chapter 별로 branch를 만들어 코드 공유
 
-* Chapter 2 - [String Calculator](https://github.com/quddnr153/next-step-java-web/tree/chapter-2-string-calculator)
+* Chapter 3 - [Web app](https://github.com/quddnr153/next-step-java-web/tree/chapter-3-web=app)
 
---
+++ src/main/java/com/bw/web/db/DataBase.java
@@ -0,0 +1,26 @@
+package com.bw.web.db;
+
+import java.util.Collection;
+import java.util.Map;
+
+import com.bw.web.model.User;
+import com.google.common.collect.Maps;
+
+/**
+ * @author Byungwook, Lee
+ */
+public class DataBase {
+	private static Map<String, User> users = Maps.newHashMap();
+
+	public static void addUser(final User user) {
+		users.put(user.getUserId(), user);
+	}
+
+	public static User findUserById(final String userId) {
+		return users.get(userId);
+	}
+
+	public static Collection<User> findAll() {
+		return users.values();
+	}
+}
