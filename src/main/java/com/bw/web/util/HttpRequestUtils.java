a07ee59734090c7ca48d992f92b3256e00eb2e18
Byungwook Lee <byungwook.lee@nhn.com>
2017-05-27 16:06:46 +0900

    [Requirements 1] index.html 응답하기 완료

--- src/main/java/com/bw/web/util/HttpRequestUtils.java
+++ src/main/java/com/bw/web/util/HttpRequestUtils.java
@@ -1,8 +1,11 @@
 package com.bw.web.util;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.StringUtils;
 
 import com.google.common.base.Strings;
 import com.google.common.collect.Maps;
@@ -11,13 +14,35 @@ import com.google.common.collect.Maps;
  * @author Byungwook, Lee
  */
 public class HttpRequestUtils {
+	private static final String SPACE_BAR = " ";
+	private static final String AND = "&";
+	private static final String SEMI_COLON = ";";
+	private static final String EQUALS = "=";
+	private static final String COLON_SPACE_BAR = ": ";
+
+	public static Map<String, String> parseRequestLine(final String requestLine) {
+		Map<String, String> result = Maps.newHashMap();
+
+		if (StringUtils.isEmpty(requestLine)) {
+			return result;
+		}
+
+		List<String> tokens = Arrays.asList(requestLine.split(SPACE_BAR));
+
+		result.put("method", tokens.get(0));
+		result.put("path", tokens.get(1));
+		result.put("version", tokens.get(2));
+
+		return result;
+	}
+
 	/**
 	 * @param queryString은
 	 *            URL에서 ? 이후에 전달되는 field1=value1&field2=value2 형식임
 	 * @return
 	 */
 	public static Map<String, String> parseQueryString(final String queryString) {
-		return parseValues(queryString, "&");
+		return parseValues(queryString, AND);
 	}
 
 	/**
@@ -26,7 +51,7 @@ public class HttpRequestUtils {
 	 * @return
 	 */
 	public static Map<String, String> parseCookies(final String cookies) {
-		return parseValues(cookies, ";");
+		return parseValues(cookies, SEMI_COLON);
 	}
 
 	private static Map<String, String> parseValues(final String values, final String separator) {
@@ -35,25 +60,8 @@ public class HttpRequestUtils {
 		}
 
 		String[] tokens = values.split(separator);
-		return Arrays.stream(tokens).map(t -> getKeyValue(t, "=")).filter(p -> p != null)
+		return Arrays.stream(tokens).map(t -> getKeyValue(t, EQUALS)).filter(p -> p != null)
 			.collect(Collectors.toMap(p -> p.getKey(), p -> p.getValue()));
-	}
-
-	static Pair getKeyValue(final String keyValue, final String regex) {
-		if (Strings.isNullOrEmpty(keyValue)) {
-			return null;
-		}
-
-		String[] tokens = keyValue.split(regex);
-		if (tokens.length != 2) {
-			return null;
-		}
-
-		return new Pair(tokens[0], tokens[1]);
-	}
-
-	public static Pair parseHeader(final String header) {
-		return getKeyValue(header, ": ");
 	}
 
 	public static class Pair {
@@ -115,5 +123,22 @@ public class HttpRequestUtils {
 		public String toString() {
 			return "Pair [key=" + key + ", value=" + value + "]";
 		}
+	}
+
+	public static Pair parseHeader(final String header) {
+		return getKeyValue(header, COLON_SPACE_BAR);
+	}
+
+	static Pair getKeyValue(final String keyValue, final String regex) {
+		if (Strings.isNullOrEmpty(keyValue)) {
+			return null;
+		}
+
+		String[] tokens = keyValue.split(regex);
+		if (tokens.length != 2) {
+			return null;
+		}
+
+		return new Pair(tokens[0], tokens[1]);
 	}
 }
--- src/main/java/com/bw/web/webserver/RequestHandler.java
+++ src/main/java/com/bw/web/webserver/RequestHandler.java
@@ -3,14 +3,21 @@
  */
 package com.bw.web.webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.bw.web.util.HttpRequestUtils;
 
 /**
  * @author Byungwook, Lee
@@ -18,9 +25,11 @@ import org.slf4j.LoggerFactory;
 public class RequestHandler extends Thread {
 	private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
+	private static final String UTF_8 = "UTF-8";
+
 	private Socket connection;
 
-	public RequestHandler(Socket connectionSocket) {
+	public RequestHandler(final Socket connectionSocket) {
 		this.connection = connectionSocket;
 	}
 
@@ -30,33 +39,42 @@ public class RequestHandler extends Thread {
 			connection.getPort());
 
 		try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-			// TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
 			DataOutputStream dos = new DataOutputStream(out);
-			byte[] body = "Hello World".getBytes();
+			byte[] body = getPage(in);
+
 			response200Header(dos, body.length);
 			responseBody(dos, body);
-		} catch (IOException e) {
-			log.error(e.getMessage());
+		} catch (IOException ioException) {
+			log.error(ioException.getMessage());
 		}
 	}
 
-	private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+	private void response200Header(final DataOutputStream dos, final int lengthOfBodyContent) {
 		try {
 			dos.writeBytes("HTTP/1.1 200 OK \r\n");
 			dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
 			dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
 			dos.writeBytes("\r\n");
-		} catch (IOException e) {
-			log.error(e.getMessage());
+		} catch (IOException ioException) {
+			log.error(ioException.getMessage());
 		}
 	}
 
-	private void responseBody(DataOutputStream dos, byte[] body) {
+	private void responseBody(final DataOutputStream dos, final byte[] body) {
 		try {
 			dos.write(body, 0, body.length);
 			dos.flush();
-		} catch (IOException e) {
-			log.error(e.getMessage());
+		} catch (IOException ioException) {
+			log.error(ioException.getMessage());
 		}
+	}
+
+	private byte[] getPage(final InputStream in) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(in, UTF_8));
+		String requestLine = br.readLine();
+
+		Map<String, String> parsedRequestLine = HttpRequestUtils.parseRequestLine(requestLine);
+
+		return Files.readAllBytes(new File("src/main/webapp" + parsedRequestLine.get("path")).toPath());
 	}
 }
