9b499c1426fc9806e64a26405dc349102b0cdc31
Byungwook Lee <byungwook.lee@nhn.com>
2017-06-05 17:07:43 +0900

    [Chapter 5] Requirements 2
    - HttpResponse 클래스로 response 메소드 취합

--- src/main/java/com/bw/web/util/HttpResponse.java
+++ src/main/java/com/bw/web/util/HttpResponse.java
@@ -1,25 +1,85 @@
 package com.bw.web.util;
 
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.file.Files;
+import java.util.Map;
+
+import com.google.common.collect.Maps;
 
 /**
  * @author Byungwook, Lee
  */
 public class HttpResponse {
+	private static final String CLASS_PATH = "src/main/webapp";
+
+	private DataOutputStream dos;
+	private Map<String, String> headerContents;
 
 	public HttpResponse(final OutputStream out) {
-
+		dos = new DataOutputStream(out);
+		headerContents = Maps.newHashMap();
 	}
 
-	public void forward(final String path) {
-
+	public void addHeader(final String key, final String value) throws IOException {
+		headerContents.put(key, value);
 	}
 
-	public void sendRedirect(final String path) {
+	public void forward(final String path) throws IOException {
+		byte[] body = getPage(path);
 
+		if (path.endsWith(".html")) {
+			headerContents.put("Content-Type", "text/html;charset=utf-8");
+		} else if (path.endsWith(".css")) {
+			headerContents.put("Content-Type", "text/css");
+		} else if (path.endsWith(".js")) {
+			headerContents.put("Content-Type", "application/javascript");
+		}
+
+		headerContents.put("Content-Length", Integer.toString(body.length));
+
+		response200Header(body.length);
+		responseBody(body);
 	}
 
-	public void addHeader(final String key, final String value) {
+	public void forwardBody(final String body) throws IOException {
+		byte[] bodyContents = body.getBytes();
 
+		headerContents.put("Content-Type", "text/html;charset=utf-8");
+		headerContents.put("Content-Length", Integer.toString(bodyContents.length));
+
+		response200Header(bodyContents.length);
+		responseBody(bodyContents);
+	}
+
+	public void sendRedirect(final String path) throws IOException {
+		dos.writeBytes("HTTP/1.1 302 Found \r\n");
+		processHeaders();
+		dos.writeBytes("Location: " + path + " \r\n");
+		dos.writeBytes("\r\n");
+	}
+
+	private void response200Header(final int length) throws IOException {
+		dos.writeBytes("HTTP/1.1 200 OK \r\n");
+		processHeaders();
+		dos.writeBytes("\r\n");
+	}
+
+	private void responseBody(final byte[] body) throws IOException {
+		dos.write(body, 0, body.length);
+		dos.writeBytes("\r\n");
+		dos.flush();
+	}
+
+	private void processHeaders() throws IOException {
+		for (String key : headerContents.keySet()) {
+			dos.writeBytes(key + ": " + headerContents.get(key) + " \r\n");
+		}
+	}
+
+	private byte[] getPage(final String pagePath) throws IOException {
+		return Files.readAllBytes(new File(CLASS_PATH + pagePath).toPath());
 	}
 }
--- src/main/java/com/bw/web/webserver/RequestHandler.java
+++ src/main/java/com/bw/web/webserver/RequestHandler.java
@@ -3,13 +3,10 @@
  */
 package com.bw.web.webserver;
 
-import java.io.DataOutputStream;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
-import java.nio.file.Files;
 import java.util.Collection;
 import java.util.Map;
 
@@ -22,6 +19,7 @@ import com.bw.web.model.User;
 import com.bw.web.service.UserService;
 import com.bw.web.util.HttpRequest;
 import com.bw.web.util.HttpRequestUtils;
+import com.bw.web.util.HttpResponse;
 
 /**
  * @author Byungwook, Lee
@@ -29,7 +27,6 @@ import com.bw.web.util.HttpRequestUtils;
 public class RequestHandler extends Thread {
 	private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
-	private static final String CLASS_PATH = "src/main/webapp";
 	private static final String INDEX_GET_PATH = "/index.html";
 	private static final String LOGIN_GET_PATH = "/user/login.html";
 	private static final String CREATE_POST_PATH = "/user/create";
@@ -38,8 +35,6 @@ public class RequestHandler extends Thread {
 	private static final String COOKIE_KEY_STRING = "Cookie";
 	private static final String LOGIN_SUCCESS_COOKIE_VALUE = "logined=true";
 	private static final String LOGIN_FAIL_COOKIE_VALUE = "logined=false";
-	private static final String DOC_TYPE_HTTP = "html";
-	private static final String DOC_TYPE_CSS = "css";
 
 	private Socket connection;
 
@@ -53,8 +48,8 @@ public class RequestHandler extends Thread {
 			connection.getPort());
 
 		try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-			DataOutputStream dos = new DataOutputStream(out);
 			HttpRequest request = new HttpRequest(in);
+			HttpResponse response = new HttpResponse(out);
 
 			String pagePath = request.getPath();
 			String cookieContent = request.getHeader(COOKIE_KEY_STRING);
@@ -73,7 +68,7 @@ public class RequestHandler extends Thread {
 
 				userService.create(user);
 
-				response302Header(dos, INDEX_GET_PATH);
+				response.sendRedirect(INDEX_GET_PATH);
 
 			} else if (pagePath.equals(LOGIN_POST_PATH)) {
 				UserService userService = new UserService();
@@ -81,94 +76,39 @@ public class RequestHandler extends Thread {
 				User user = userService.makeUser(request);
 
 				if (userService.login(user)) {
-					response302HeaderIncludedCookie(dos, INDEX_GET_PATH, LOGIN_SUCCESS_COOKIE_VALUE);
+					response.addHeader("Set-Cookie", LOGIN_SUCCESS_COOKIE_VALUE);
+					response.sendRedirect(INDEX_GET_PATH);
 
 				} else {
 					log.debug("user login fails");
 
-					response302HeaderIncludedCookie(dos, INDEX_GET_PATH, LOGIN_FAIL_COOKIE_VALUE);
+					response.addHeader("Set-Cookie", LOGIN_FAIL_COOKIE_VALUE);
+					response.sendRedirect(INDEX_GET_PATH);
 
 				}
 
 			} else if (pagePath.equals(LOGIN_GET_PATH)) {
-				byte[] body = getPage(pagePath);
-
-				response200Header(dos, DOC_TYPE_HTTP, body.length);
-				responseBody(dos, body);
+				response.forward(pagePath);
 
 			} else if (pagePath.equals(USER_LIST_POST_PATH)) {
 				if (!isLogined) {
-					response302Header(dos, LOGIN_GET_PATH);
+					response.sendRedirect(LOGIN_GET_PATH);
 
 					return;
 				}
 
-				byte[] body = getUserListToByte();
-
-				response200Header(dos, DOC_TYPE_HTTP, body.length);
-				responseBody(dos, body);
+				response.forwardBody(getUsers());
 
 			} else if (pagePath.endsWith(".css")) {
-				byte[] body = getPage(pagePath);
-
-				response200Header(dos, DOC_TYPE_CSS, body.length);
-				responseBody(dos, body);
+				response.forward(pagePath);
 
 			} else {
-				byte[] body = getPage(pagePath);
-
-				response200Header(dos, DOC_TYPE_HTTP, body.length);
-				responseBody(dos, body);
+				response.forward(pagePath);
 
 			}
 		} catch (IOException ioException) {
 			log.error(ioException.getMessage());
 		}
-	}
-
-	private void response200Header(final DataOutputStream dos, final String docType, final int lengthOfBodyContent) {
-		try {
-			dos.writeBytes("HTTP/1.1 200 OK \r\n");
-			dos.writeBytes("Content-Type: text/" + docType + ";charset=utf-8\r\n");
-			dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-			dos.writeBytes("\r\n");
-		} catch (IOException ioException) {
-			log.error(ioException.getMessage());
-		}
-	}
-
-	private void response302Header(final DataOutputStream dos, final String redirectPath) {
-		try {
-			dos.writeBytes("HTTP/1.1 302 Found \r\n");
-			dos.writeBytes("Location: " + redirectPath + " \r\n");
-		} catch (IOException ioException) {
-			log.error(ioException.getMessage());
-		}
-	}
-
-	private void response302HeaderIncludedCookie(final DataOutputStream dos, final String redirectPath,
-		final String cookie) {
-		try {
-			dos.writeBytes("HTTP/1.1 302 Found \r\n");
-			dos.writeBytes("Location: " + redirectPath + " \r\n");
-			dos.writeBytes("Set-Cookie: " + cookie + "; Path=/ \r\n");
-			dos.writeBytes("\r\n");
-		} catch (IOException ioException) {
-			log.error(ioException.getMessage());
-		}
-	}
-
-	private void responseBody(final DataOutputStream dos, final byte[] body) {
-		try {
-			dos.write(body, 0, body.length);
-			dos.flush();
-		} catch (IOException ioException) {
-			log.error(ioException.getMessage());
-		}
-	}
-
-	private byte[] getPage(final String pagePath) throws IOException {
-		return Files.readAllBytes(new File(CLASS_PATH + pagePath).toPath());
 	}
 
 	private boolean getLoginCookieToBoolean(final String cookieValue) {
@@ -179,7 +119,7 @@ public class RequestHandler extends Thread {
 		}
 	}
 
-	private byte[] getUserListToByte() {
+	private String getUsers() {
 		Collection<User> users = DataBase.findAll();
 
 		StringBuilder sb = new StringBuilder();
@@ -194,6 +134,6 @@ public class RequestHandler extends Thread {
 		}
 		sb.append("</table>");
 
-		return sb.toString().getBytes();
+		return sb.toString();
 	}
 }
