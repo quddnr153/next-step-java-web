49531cd951c5bb0fbbe09a3dec583f096cfc97e9
Byungwook Lee <quddnr153@gmail.com>
2017-06-06 22:40:02 +0900

    [Chapter 6] 프로젝트 설정

---
+++ src/test/java/com/bw/jwp/next/dao/UserDaoTest.java
@@ -0,0 +1,21 @@
+package com.bw.jwp.next.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.bw.jwp.next.model.User;
+
+public class UserDaoTest {
+
+	@Test
+	public void crud() throws Exception {
+		User expected = new User("userId", "password", "name", "javajigi@email.com");
+		UserDao userDao = new UserDao();
+		userDao.insert(expected);
+
+		User actual = userDao.findByUserId(expected.getUserId());
+		assertEquals(expected, actual);
+	}
+
+}
